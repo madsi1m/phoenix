09c368fa4335d274de5c75520a573baa9417c691
Vincent Petry <pvince81@owncloud.com>
2019-12-06 16:52:51 +0100

    Ability to package top bar as web component

--- src/components/Top-Bar.vue
+++ src/components/Top-Bar.vue
@@ -1,26 +1,33 @@
 <template>
-  <oc-navbar id="oc-topbar" tag="header" class="oc-topbar uk-position-relative uk-navbar">
-    <oc-navbar-item position="left">
-      <oc-button v-if="hasAppNavigation" icon="menu" variation="primary" class="oc-topbar-menu-burger uk-height-1-1" aria-label="Menu" @click="$_onOpenAppNavigation" ref="menubutton">
-        <span class="oc-topbar-menu-burger-label" v-translate>Menu</span>
-      </oc-button>
-    </oc-navbar-item>
-    <oc-navbar-item position="center">
-      <router-link to="/" class="oc-topbar-icon">ownCloud X</router-link>
-    </oc-navbar-item>
-    <oc-navbar-item position="right" v-if="!isPublicPage">
-      <notifications v-if="activeNotifications.length" />
-      <applications-menu :applicationsList="applicationsList"/>
-      <user-menu :user-id="userId" :user-display-name="userDisplayName" />
-    </oc-navbar-item>
-  </oc-navbar>
+  <div>
+    <link v-if="phoenixUrl" rel="stylesheet" :href="phoenixUrl + '/dist-wc/design-system/system.css'" />
+    <oc-navbar id="oc-topbar" tag="header" class="oc-topbar uk-position-relative uk-navbar">
+      <oc-navbar-item position="left">
+        <oc-button v-if="hasAppNavigation" icon="menu" variation="primary" class="oc-topbar-menu-burger uk-height-1-1" aria-label="Menu" @click="$_onOpenAppNavigation" ref="menubutton">
+          <span class="oc-topbar-menu-burger-label" v-translate>Menu</span>
+        </oc-button>
+      </oc-navbar-item>
+      <oc-navbar-item position="center">
+        <router-link to="/" class="oc-topbar-icon">ownCloud X</router-link>
+      </oc-navbar-item>
+      <oc-navbar-item position="right" v-if="!isPublicPage">
+        <notifications v-if="activeNotifications.length" />
+        <applications-menu :applicationsList="applicationsList"/>
+        <user-menu :user-id="userId" :user-display-name="userDisplayName" />
+      </oc-navbar-item>
+    </oc-navbar>
+  </div>
 </template>
 
 <script>
+// import externalComponent from '../utils/external-component.js'
 import pluginHelper from '../mixins/pluginHelper.js'
 import ApplicationsMenu from './ApplicationsMenu.vue'
 import UserMenu from './UserMenu.vue'
 import Notifications from './Notifications.vue'
+// FIXME: replace with dynamic load with externalComponent()
+/* eslint-disable no-unused-vars */
+import DesignSystem from 'owncloud-design-system'
 
 export default {
   mixins: [
@@ -56,6 +63,10 @@ export default {
       type: [Array, Boolean],
       required: false,
       default: () => []
+    },
+    phoenixUrl: {
+      type: String,
+      required: false
     }
   },
   methods: {
